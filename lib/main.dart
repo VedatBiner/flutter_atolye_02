d5baf1ca9a85f24dbe09ce450f265f8bb1868433
Vedat Biner <vbiner@gmail.com>
2022-08-04 08:05:46 +0300

    Veri akışı

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-// Ekran tasarımı denemeleri
-// Row, expanded, flesible, spacer
+// Tasarım Örneği - Biraz daha Widget ağacı
+// Veri akışı
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,7 +9,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,134 +24,168 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
+
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
   @override
   void initState() {
     super.initState();
-    // tek sefer çalışacağı kabul edilebilir bir kullanım
-    // ogrenciler.add("initState 'den");
+    ogrenciler.add("init");
   }
+
   var sinif = 5;
   var baslik = "Öğrenciler";
-  var ogrenciler = ["Ali", "Ayşe", "Can"];
+  var ogrenciler = ["Mehmet", "Vedat", "Zeynep"];
+
+  void yeniOgrenciEkle(String yeniOgrenci){
+    setState(() {
+      ogrenciler.add(yeniOgrenci);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    print("MyHomePageState Build...");
+    print("MyHomePageState Build ...");
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+      body: Sinif(
+        sinif: sinif,
+        baslik: baslik,
+        ogrenciler: ogrenciler,
+        yeniOgrenciEkle: yeniOgrenciEkle,
+
+      ),
+    );
+  }
+}
+
+class Sinif extends StatelessWidget {
+  const Sinif({
+    Key? key,
+    required this.sinif,
+    required this.baslik,
+    required this.ogrenciler,
+    required this.yeniOgrenciEkle,
+  }) : super(key: key);
+
+  final int sinif;
+  final String baslik;
+  final List<String> ogrenciler;
+  final void Function(String yeniOgrenci) yeniOgrenciEkle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Icon(
-                  Icons.star,
-                  color: Colors.red,
-                ),
-                Text(
-                  "$sinif. Sınıf",
-                  textScaleFactor: 2,
-                ),
-                const Icon(
-                  Icons.star,
-                  color: Colors.red,
-                )
-              ],
+          children: [
+            const Icon(
+              Icons.star,
+              color: Colors.red,
             ),
             Text(
-              baslik,
-              textScaleFactor: 1.5,
+              "$sinif. Sınıf",
+              textScaleFactor: 2,
             ),
-            for (final o in ogrenciler)
-              Text(
-                  o
-              ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    ogrenciler.add("Yeni");
-                  });
-                },
-                child: const Text(
-                  "Ekle",
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 100,
-              height: 200,
-              child: Container(
-                color: Colors.yellow,
-                child: Center(
-                  child: Container(
-                    color: Colors.blue,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-                      child: Container(
-                        width: 50,
-                        height: 100,
-                        color: Colors.red,
-                        child: const Center(
-                          child: Text(
-                            "AAA",
-                            textAlign: TextAlign.right,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Row(
-                children: const [
-                  Flexible(
-                    child:
-                    Text(
-                        "Flexible"
-                    ),
-                  ),
-                  Flexible(
-                    child:
-                    Text(
-                        "Flexible"
-                    ),
-                  ),
-                  Expanded(
-                    child: Text(
-                        "Expanded"
-                    ),
-                  ),
-                  Text(
-                      "Merhaba ve spacer"
-                  ),
-                  Spacer(),
-                  Text(
-                      "Merhaba"
-                  ),
-                  Text(
-                      "Merhaba"
-                  ),
-                ]
+            const Icon(
+              Icons.star,
+              color: Colors.red,
             ),
           ],
         ),
-      ),
+        Text(
+          baslik,
+          textScaleFactor: 1.5,
+        ),
+        OgrenciListesi(ogrenciler: ogrenciler),
+        OgrenciEkleme(yeniOgrrenciEkle: yeniOgrenciEkle),
+      ],
+    );
+  }
+}
+
+class OgrenciListesi extends StatelessWidget {
+  const OgrenciListesi({
+    Key? key,
+    required this.ogrenciler,
+  }) : super(key: key);
+
+  final List<String> ogrenciler;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children:[
+        for (final o in ogrenciler)
+          Text(
+              o
+          ),
+      ],
+    );
+  }
+}
+
+class OgrenciEkleme extends StatefulWidget {
+
+  const OgrenciEkleme({
+    Key? key, required this.yeniOgrrenciEkle,
+  }) : super(key: key);
+
+  final void Function(String yeniOgrenci) yeniOgrrenciEkle;
+
+  @override
+  State<OgrenciEkleme> createState() => _OgrenciEklemeState();
+}
+
+class _OgrenciEklemeState extends State<OgrenciEkleme> {
+  final controller = TextEditingController();
+
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        TextField(
+          controller: controller,
+          onChanged: (value) {
+            setState(() {
+
+            });
+          },
+        ),
+        ElevatedButton(
+          onPressed: controller.text.isEmpty? null :  (){
+            final yeniOgrenci = controller.text;
+            widget.yeniOgrrenciEkle(yeniOgrenci);
+            controller.text = "";
+            // setState(() {
+            //   ogrenciler.add("yeni");
+            //  });
+          },
+          child: const Text(
+            "Ekle",
+          ),
+        ),
+      ],
     );
   }
 }
